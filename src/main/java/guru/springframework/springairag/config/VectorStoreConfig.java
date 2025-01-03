a6f555a9611b35a9cc5d09e32c1f1f91bc7035cf
Eduardo Morbey <eduardomorbey@gmail.com>
2025-01-03 23:19:25 +0000

    SpringAi with RAG

---
+++ src/main/java/guru/springframework/springairag/config/VectorStoreConfig.java
@@ -0,0 +1,44 @@
+package guru.springframework.springairag.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.util.List;
+
+@Slf4j
+@Configuration
+public class VectorStoreConfig {
+
+    @Bean
+    public SimpleVectorStore simpleVectorStore(EmbeddingModel embeddingModel, VectorStoreProperties vectorStoreProperties) {
+        var store = SimpleVectorStore.builder(embeddingModel).build();
+        File vectorStoreFile = new File(vectorStoreProperties.getVectorStorePath());
+
+        if (vectorStoreFile.exists()) {
+            store.load(vectorStoreFile);
+        } else {
+            log.debug("Loading documents into vector store");
+            vectorStoreProperties.getDocumentsToLoad().forEach(document -> {
+                log.debug("Loading document: {}", document.getFilename());
+                TikaDocumentReader documentReader = new TikaDocumentReader(document);
+                List<Document> docs = documentReader.get();
+                TextSplitter splitter = new TokenTextSplitter();
+                List<Document> splitDocuments = splitter.split(docs);
+                store.add(splitDocuments);
+            });
+
+            store.save(vectorStoreFile);
+        }
+
+        return store;
+    }
+
+}
